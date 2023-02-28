304455539b0807a1463027ede4a054ee87b882af
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2023-02-28 09:42:36 +0600

    [feat]: filter by todo status

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,18 +2,28 @@ import { useSelector } from "react-redux";
 import Todo from "./Todo";
 
 export default function TodoList() {
+  const todos = useSelector((state) => state.todos);
+  const filters = useSelector((state) => state.filters);
 
-const todos = useSelector((state)=> state.todos)
+  return (
+    <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
+      {/* <Todo /> */}
+      {todos
+        .filter((todo) => {
+          const { status } = filters;
+          switch (status) {
+            case "Complete":
+              return todo.completed;
+            case "Incomplete":
+              return !todo.completed;
 
-    return (
-        <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-            {/* <Todo /> */}
-            {
-                todos.map((todo) => (<Todo
-                    key={todo.id}
-                    todo={todo}
-                />))
-            }
-        </div>
-    );
+            default:
+              return true;
+          }
+        })
+        .map((todo) => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
+    </div>
+  );
 }
