18a99ef461c77c4f772550b54bdf1fc7a699c779
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2023-02-28 09:06:35 +0600

    [feat]: toto actions

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,86 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  filterColorChanged,
+  filterStatusChanged,
+} from "../redux/filters/actionCreators";
+
+const numberOfTodos = (number_of_task) => {
+  switch (number_of_task) {
+    case 0:
+      return "YAY!! No Task";
+    case 1:
+      return "1 task";
+    default:
+      return `${number_of_task} tasks`;
+  }
+};
+
 export default function Footer() {
-    return (
-        <div className="mt-4 flex justify-between text-xs text-gray-500">
-            <p>2 tasks left</p>
-            <ul className="flex space-x-1 items-center text-xs">
-                <li className="cursor-pointer font-bold">All</li>
-                <li>|</li>
-                <li className="cursor-pointer">Incomplete</li>
-                <li>|</li>
-                <li className="cursor-pointer">Complete</li>
-                <li></li>
-                <li></li>
-                <li className="h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer bg-green-500"></li>
-                <li className="h-3 w-3 border-2 border-red-500 md:hover:bg-red-500 rounded-full cursor-pointer"></li>
-                <li className="h-3 w-3 border-2 border-yellow-500 md:hover:bg-yellow-500 rounded-full cursor-pointer"></li>
-            </ul>
-        </div>
-    );
+  const todos = useSelector((state) => state.todos);
+  const filters = useSelector((state) => state.filters);
+  const { status, colors } = filters;
+
+  const remainingTodos = todos.filter((todo) => !todo.completed).length;
+
+  const dispatch = useDispatch();
+
+  const handleStatusChange = (status) => {
+    dispatch(filterStatusChanged(status));
+  };
+
+  const handleColorChange = (color) => {
+    if (colors.includes(color)) {
+      dispatch(filterColorChanged("removed", color));
+    } else {
+      dispatch(filterColorChanged("added", color));
+    }
+  };
+
+  return (
+    <div className="mt-4 flex justify-between text-xs text-gray-500">
+      <p>{numberOfTodos(remainingTodos)} left</p>
+      <ul className="flex space-x-1 items-center text-xs">
+        <li
+          onClick={() => handleStatusChange("All")}
+          className={`cursor-pointer ${status === "All" && "font-bold"}`}
+        >
+          All
+        </li>
+        <li>|</li>
+        <li
+          onClick={() => handleStatusChange("Incomplete")}
+          className={`cursor-pointer ${status === "Incomplete" && "font-bold"}`}
+        >
+          Incomplete
+        </li>
+        <li>|</li>
+        <li
+          onClick={() => handleStatusChange("Complete")}
+          className={`cursor-pointer ${status === "Complete" && "font-bold"}`}
+        >
+          Complete
+        </li>
+        <li></li>
+        <li></li>
+        <li
+          onClick={() => handleColorChange("green")}
+          className={`h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer  ${
+            colors.includes("green") && "bg-green-500"
+          }`}
+        ></li>
+        <li
+          onClick={() => handleColorChange("red")}
+          className={`h-3 w-3 border-2 border-red-500 md:hover:bg-red-500 rounded-full cursor-pointer  ${
+            colors.includes("red") && "bg-red-500"
+          }`}
+        ></li>
+        <li
+          onClick={() => handleColorChange("yellow")}
+          className={`h-3 w-3 border-2 border-yellow-500 md:hover:bg-yellow-500 rounded-full cursor-pointer  ${
+            colors.includes("yellow") && "bg-yellow-500"
+          }`}
+        ></li>
+      </ul>
+    </div>
+  );
 }
